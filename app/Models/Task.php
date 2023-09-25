64596cb9d6d793bc1010e02ed963532a4223d246
Ntiyiso Rikhotso <truminnd@gmail.com>
2023-09-25 10:07:19 +0200

    Create Task.php

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'due_date' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
