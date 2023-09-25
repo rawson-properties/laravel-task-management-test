90f39b02af9664dbfc4eedaaa812fbab7a24bbd1
Ntiyiso Rikhotso <truminnd@gmail.com>
2023-09-25 10:08:40 +0200

    Create welcome.blade.php

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+<body>
+<h1>Welcome to Our Website, {{ $user->name }}!</h1>
+<p>Thank you for registering on our platform.</p>
+</body>
+</html>
