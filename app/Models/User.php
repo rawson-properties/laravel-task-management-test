beef6d33e72edba6143eb221a7bacff146b6b8de
Ntiyiso Rikhotso <truminnd@gmail.com>
2023-09-25 10:07:03 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Jobs\SendWelcomeEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property Collection $tasks
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,4 +55,30 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::created(fn (User $user) => SendWelcomeEmail::dispatch($user));
+        static::deleting(fn (User $user) => $user->tasks()->delete());
+    }
+
+    public function password(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $value,
+            set: fn (string $value) => bcrypt($value),
+        );
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function resolveRouteBinding($value, $field = null): Model|Relation|null
+    {
+        return $this->resolveRouteBindingQuery($this, Auth::id(), $field)->first();
+    }
 }
